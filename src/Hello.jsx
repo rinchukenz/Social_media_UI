50a5ed535306901b148d7e43de48b49c73bd2710
rinchukenz <rinchu9999@gmail.com>
2025-03-17 21:52:56 +0530

    initial commit

---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/2462015/pexels-photo-2462015.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+    </div>
+  )
+}
+
+export default Hello
